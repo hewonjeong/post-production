8d185a66474358b785f10a4bbddfa19df0a14579
심철환 <a@akaiv.com>
2018-10-17 14:37:12 +0900

    디자인: 레이아웃 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,2 +1,8 @@
-const App = () => null
+import React from 'react'
+import Layout from './components/Layout'
+
+const App = () => (
+  <Layout header="Title" content="Preview" aside="Panel" footer="Track" />
+)
+
 export default App
---
+++ src/components/Layout.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { node } from 'prop-types'
+
+const propTypes = { header: node, content: node, aside: node, footer: node }
+const defaultProps = { header: null, content: null, aside: null, footer: null }
+
+const Layout = ({ header, content, aside, footer }) => (
+  <>
+    <header style={style.header}>{header}</header>
+
+    <main style={style.main}>
+      <section style={style.content}>{content}</section>
+      <aside style={style.aside}>{aside}</aside>
+    </main>
+
+    <footer style={style.footer}>{footer}</footer>
+  </>
+)
+
+Layout.propTypes = propTypes
+Layout.defaultProps = defaultProps
+
+const borderBottom = '2px solid hsl(0, 0%, 15%)'
+const backgroundColor = 'hsl(0, 0%, 20%)'
+const style = {
+  header: { flex: 'none', height: 40, backgroundColor, borderBottom },
+  main: { flex: 1, display: 'flex' },
+  content: { flex: 1 },
+  aside: { flex: 'none', width: 400, backgroundColor },
+  footer: { flex: 'none', height: 250 }
+}
+
+export default Layout
