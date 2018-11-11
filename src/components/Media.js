eba3a0b40640e38d4989e3107c9ac3703369a0c9
심철환 <a@akaiv.com>
2018-11-11 18:39:50 +0900

    기능: 미디어의 시간과 파일이름 출력

--- src/components/Media.js
+++ src/components/Media.js
@@ -12,27 +12,40 @@ class Media extends React.Component {
   getStyle = () => ({ ...style.component, height: (this.state.width * 9) / 16 })
 
   render() {
-    const { thumbnail, filename, overlay } = this.props
+    const { thumbnail, filename, duration, overlay } = this.props
     return (
       <article style={this.getStyle()} ref={this.component}>
         <img src={thumbnail} alt={filename} style={style.image} />
-        {overlay && <p style={style.overlay}>{overlay}</p>}
+        {overlay ? (
+          <p style={style.overlay}>{overlay}</p>
+        ) : (
+          <>
+            <h1 style={style.filename}>{filename}</h1>
+            <p style={style.duration}>{Math.ceil(duration)}초</p>
+          </>
+        )}
       </article>
     )
   }
 }
 
-const absolute = { position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }
+const absolute = {
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  backgroundColor: 'hsla(0, 0%, 0%, .5)',
+  color: 'white'
+}
+
 const flex = { display: 'flex', justifyContent: 'center', alignItems: 'center' }
 const style = {
   component: { borderRadius: 4, overflow: 'hidden', position: 'relative' },
   image: { width: '100%', height: 'auto' },
-  overlay: {
-    ...absolute,
-    ...flex,
-    backgroundColor: 'hsla(0, 0%, 0%, .5)',
-    color: 'white'
-  }
+  filename: { ...absolute, top: 'auto' },
+  duration: { ...absolute, bottom: 'auto', right: 'auto' },
+  overlay: { ...absolute, ...flex }
 }
 
 export default Media
