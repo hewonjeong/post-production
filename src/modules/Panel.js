74dc9b9083ab9cdfd1e5e7f2fed4aa13deb467e1
심철환 <a@akaiv.com>
2018-11-11 18:41:04 +0900

    변경: 패널에서 Share 제거

--- src/modules/Panel.js
+++ src/modules/Panel.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import Tab from '../layout/Tab'
 import Assets from '../modules/Assets'
-import Export from '../modules/Export'
 
-const Panel = () => (
-  <Tab
-    list={[
-      { name: 'Assets', component: <Assets /> },
-      { name: 'Share', component: <Export /> }
-    ]}
-  />
-)
+const Panel = () => <Tab list={[{ name: 'Assets', component: <Assets /> }]} />
 
 export default Panel
