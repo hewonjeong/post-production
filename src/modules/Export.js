ac83f52f1c489f3f4d445af93306bfb984bab292
심철환 <a@akaiv.com>
2018-11-26 01:05:54 +0900

    기능: 미리보기 캔버스에서 녹화

--- src/modules/Export.js
+++ src/modules/Export.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const Export = ({ url }) =>
-  url ? (
-    <>
-      <video src={url} style={style.video} autoPlay loop controls />
-      <a href={url} download="video.webm" style={style.download}>
-        Download
-      </a>
-    </>
-  ) : (
-    'Recording...'
-  )
+const Export = ({ url }) => (
+  <div style={style.component}>
+    {url ? (
+      <>
+        <video src={url} style={style.video} autoPlay loop controls />
+        <p>
+          <a href={url} download="video.webm" style={style.download}>
+            Download
+          </a>
+        </p>
+      </>
+    ) : (
+      'Recording...'
+    )}
+  </div>
+)
 
 const style = {
   component: { padding: 16 },
